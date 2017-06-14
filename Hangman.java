267cfc05836095ffe28abd29ee024559eaeba30f
khwang1994 <khwang1994@yahoo.com>
2017-06-13 21:34:28 -0400

    Implemented magicWord() method

--- Hangman.java
+++ Hangman.java
@@ -4,7 +4,8 @@ import java.io.*;
 public class Hangman {
 
 	WordList words;
-	String magic_word;
+	char[] magic; 
+	int magic_len;
 	int num_wrong_guess;
 	int num_rounds;
 	int num_wins;
@@ -13,18 +14,32 @@ public class Hangman {
 
 	Hangman(){
 		words = null;
-		magic_word = null;
 	}
 
 	Hangman(WordList words){
 		this.words = words;
-		magic_word = this.words.getMagicWord();
 		num_rounds = 1;
 		num_wins = 0;
+		magicWord();
+	}
+
+	// Generates the magic word from the WordList, then converts the word
+	// to an easily accessible array and store as Hangman object's 
+	// char magic[]
+	void magicWord(){
+		String word = this.words.getMagicWord();
+		this.magic_len = word.length();
+		this.magic = new char[magic_len];
+
+		// Iterates through word and stores in char magic[]
+		for(int i = 0; i < word.length(); i++){
+			magic[i] = word.charAt(i);
+		}
+
 	}
 
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		// Create WordList object using input dictionary File
 		WordList list = new WordList(new File(args[0]));
 		// Create Hangman object using WordList object
@@ -38,7 +53,8 @@ public class Hangman {
 		while(num_wrong_guess <= guess_limit){
 			System.out.println("Guess a letter: ");
 			Scanner in = new Scanner(System.in);
-			char guess = in.next().toLower();
+			String guess = in.next().toLowerCase();
+
 		}
 
 	}
